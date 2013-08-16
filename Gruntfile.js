3d67da005b2820e81ad4fbf5fd75c4034b2cce36
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2013-08-15 22:52:51 -0300

    Banner in dist file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '// <%= pkg.name %> v<%= pkg.version %>\n' +
+                '// (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '// Distributed Under MIT License\n',
+      },
+      dist: {
+        src: ['dist/fnky.js'],
+        dest: 'dist/fnky.js',
+      },
+    },
     browserify: {
       'dist/fnky.js': ['index.js'],
       options: {
@@ -27,6 +40,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-simple-mocha');
@@ -35,9 +49,9 @@ module.exports = function(grunt) {
     require('./test/chai');
   });
 
-  grunt.task.registerTask('test:client', ['browserify', 'mocha']);
+  grunt.task.registerTask('test:client', ['build', 'mocha']);
   grunt.task.registerTask('test:server', ['chai-globals', 'simplemocha']);
   grunt.task.registerTask('test:all', ['test:server', 'test:client']);
-  grunt.registerTask('build', ['browserify']);
+  grunt.registerTask('build', ['browserify', 'concat']);
   grunt.registerTask('default', ['test:server']);
 };
